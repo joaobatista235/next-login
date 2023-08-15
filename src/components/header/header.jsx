4f565b144f986ac0329d963d29b14e5163da05ed
Junior <alexanroju@hotmail.com>
2023-08-15 18:04:44 -0300

    header adicionado

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+'use client'
 
-const header = () => {
+import React, { useState } from 'react'
+import { Item, Logo, Menu, Nav } from './Styles'
+import Link from 'next/link'
+import Image from 'next/image'
+import { AnimatePresence, motion } from 'framer-motion'
+const Header = () => {
+  const items = [
+    { url: '/', name: 'Inicio' },
+    { url: '/', name: 'Sobre' },
+    { url: '', name: 'Serviços' },
+    { url: '/', name: 'Contato' },
+  ]
+
+  const [open, setOpen] = useState()
+  const anim = {
+    exit: {
+      opacity: 0,
+      heigth: 0,
+      transition: {
+        ease: 'easeInOut',
+        duration: 0.3,
+        delay: 0.9,
+
+      }
+    }
+  }
+
   return (
-    <div>header</div>
+    <Nav>
+      <Logo>
+        <h1>Medico</h1>
+      </Logo>
+      <Menu>
+        <div className="desktop">
+          {items.map((index) => (
+            <Item>
+              <Link href={index.url}>
+                {index.name}
+              </Link>
+            </Item>
+          ))}
+        </div>
+
+        <div className='mobile'>
+          <Image src={'/menu.png'} alt='' width={20} height={20} onClick={() => setOpen(!open)} />
+          <AnimatePresence>
+            {open && (
+              <motion.div className="popup"
+                variants={anim}
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: '100vh', opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                exit='exit'
+              >
+                <Image src={'/x.png'} alt='' width={20} height={20} onClick={() => setOpen(!open)} />
+
+                <Item
+                  variants={anim}
+                  initial={{ y: 90, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                  exit={{
+                    opacity: 0,
+                    y: 90,
+                    transition: {
+                      ease: 'easeInOut',
+                      delay: 0.8,
+                    }
+                  }}
+                >
+                  <Link href={'/'}>
+                    Inicio
+                  </Link>
+                </Item>
+                <Item
+                  variants={anim}
+                  initial={{ y: 90, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                  exit={{
+                    opacity: 0,
+                    y: 90,
+                    transition: {
+                      ease: 'easeInOut',
+                      delay: 0.6,
+                    }
+                  }}
+                >
+                  <Link href={'/'}>
+                    Sobre
+                  </Link>
+                </Item>
+                <Item
+                  variants={anim}
+                  initial={{ y: 90, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                  exit={{
+                    opacity: 0,
+                    y: 90,
+                    transition: {
+                      ease: 'easeInOut',
+                      delay: 0.4,
+                    }
+                  }}
+                >
+                  <Link href={'/'}>
+                    Serviços
+                  </Link>
+                </Item>
+                <Item
+                  variants={anim}
+                  initial={{ y: 90, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                  exit={{
+                    opacity: 0,
+                    y: 90,
+                    transition: {
+                      ease: 'easeInOut',
+                      delay: 0.2,
+                    }
+                  }}
+                >
+                  <Link href={'/'}>
+                    Contato
+                  </Link>
+                </Item>
+
+              </motion.div>
+            )}
+          </AnimatePresence>
+
+        </div>
+      </Menu>
+    </Nav>
   )
 }
 
-export default header
+export default Header
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,1 +1,66 @@
-export const 
+import { styled } from "styled-components";
+import { motion } from "framer-motion";
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 60px;
+    padding: 0 80px;
+`
+
+export const Logo = styled.div`
+
+`
+
+export const Menu = styled.div`
+
+    &> .desktop{
+        display: flex;
+    }
+    & > .mobile{
+        display: none;
+    }
+    @media screen and (max-width: 900px) {
+        &> .desktop{
+            display: none;
+        }
+        & > .mobile{
+
+            display: flex;
+       
+            & > .popup{
+                position: absolute;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding: 1rem;
+                background-color: white;
+                box-shadow: 0 0 10px rgba(0,0,0,0.10);
+                top: 0;
+                right: 0;
+                width: 100%;
+                height: 100vh;
+                border-radius: 10px;
+
+                & > img{
+                    position: absolute;
+                    display: flex;
+                    justify-content: flex-end;
+                    top: 10px;
+                    right: 10px;
+                }
+            }
+        }
+      
+    }
+`
+
+export const Item = styled(motion.li)`
+    margin-left: 20px;
+    font-size: 22px;
+    font-weight: 500;
+
+    @media screen and (max-width: 900px) {
+      margin: 0;
+    }
+`
